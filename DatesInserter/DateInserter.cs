4938316f4b10b7d7a30585d324f836e2e248500d
warnov <warnov@gmail.com>
2016-03-23 06:35:10 -0500

    First commit. Working ok.

---
+++ DatesInserter/DateInserter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace DatesInserter
+{
+    public partial class DateInserter
+    {
+        private static Microsoft.Office.Interop.Excel.Application 
+            App => Globals.ThisAddIn.Application;
+        private void btnToday_Click(object sender, RibbonControlEventArgs e)
+        {
+            App.ActiveCell.Value = DateTime.Today;
+        }
+
+        private void btnNextWeek_Click(object sender, RibbonControlEventArgs e)
+        {
+            App.ActiveCell.Value = DateTime.Today + TimeSpan.FromDays(7);
+        }
+
+        private void btnPreviousWeek_Click(object sender, RibbonControlEventArgs e)
+        {
+            App.ActiveCell.Value = DateTime.Today - TimeSpan.FromDays(7);
+        }
+
+        private void txtDelta_TextChanged(object sender, RibbonControlEventArgs e)
+        {
+            int delta;
+            if (int.TryParse(txtDelta.Text, out delta))
+            {
+                 App.ActiveCell.Value =  DateTime.Today + TimeSpan.FromDays(delta);
+            }
+
+        }
+    }
+}
